41099e587e778e714df99ae8c43e0414d97a98be
Graham Ashton <graham@effectif.com>
2024-11-08 09:48:03 +0000

    Make install log more prominent

--- src/playwright/install.sh
+++ src/playwright/install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-echo "Installing dependencies for ${BROWSERS}"
+echo "## Installing dependencies for ${BROWSERS}"
 
 su "${_REMOTE_USER}" -c "npm install -g playwright"
 su "${_REMOTE_USER}" -c "npx playwright install-deps ${BROWSERS}"
