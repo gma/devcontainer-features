905dc7b9ce6819fd8bb3a3bc56c1ddd7ae5f9cf7
Graham Ashton <graham@effectif.com>
2024-11-08 09:15:07 +0000

    Install Playwright as container's remote user
    We were previously installing them as the "container user", which
    equates to whichever user was specified in the Dockerfile, defaulting to
    root.
    
    Dev containers are often setup to run processes within the container as
    a separate user, set via the remoteUser property in devcontainer.json.
    It's often set to "vscode".
    
    It's typical to run npm and npx as a non-root user, hence this change.
    Note that `npx playwright install-deps` runs sudo automatically.

--- src/playwright/install.sh
+++ src/playwright/install.sh
@@ -4,6 +4,6 @@ set -e
 
 echo "Installing dependencies for ${BROWSERS}"
 
-su "${_CONTAINER_USER}" -c "npm install -g playwright"
-su "${_CONTAINER_USER}" -c "npx playwright install-deps ${BROWSERS}"
-su "${_CONTAINER_USER}" -c "npm uninstall -g playwright"
+su "${_REMOTE_USER}" -c "npm install -g playwright"
+su "${_REMOTE_USER}" -c "npx playwright install-deps ${BROWSERS}"
+su "${_REMOTE_USER}" -c "npm uninstall -g playwright"
